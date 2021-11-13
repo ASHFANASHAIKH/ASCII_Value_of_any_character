39481f970213df5f05234295ff449bfb261e08aa
Ashfana Ashfu <shaikhashfana9@gmail.com>
2021-11-13 17:06:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Value_of_any_character
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("Enter any  character:\n");
+            s = Console.ReadLine();
+            foreach (var c in s)
+            {
+                Console.Write((int)c);
+                Console.Write("\n");
+            }
+        }
+    }
+}
